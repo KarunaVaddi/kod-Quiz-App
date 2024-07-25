062a0904cadb6f0a5bc94f0a043dc277429c54fd
KarunaVaddi <vaddi.karuna12@gmail.com>
2024-07-25 14:45:55 +0530

    1st version of quiz app

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import './Quiz.css'
+import { data } from '../../data'
+
+
+export default function Quiz() {
+  const [index, setIndex] = React.useState(0);
+  const [question, setQuestion] = React.useState(data[index]);
+  
+  //for checking questions is last or not
+  const [isLastPage, setIsLastPage] = React.useState(false);
+
+  const [score,setScore] = React.useState(0);
+
+  const [lock, setLock] = React.useState(false);
+
+  function nextQuestion() {
+    // Remove 'correct' and 'incorrect' classes from previous answer
+    const answerItems = document.querySelectorAll('.quiz ul li');
+    answerItems.forEach(item => {
+      item.classList.remove('correct', 'incorrect');
+    });
+  
+    // Move to the next question
+    if (index < data.length - 1) {
+      setIndex(index + 1);
+      setQuestion(data[index + 1]);
+    } else {
+      setIsLastPage(true);
+    }
+  
+    // Reset lock state
+    setLock(false);
+  }
+  
+
+function checkAnswer(e,ans)
+  { 
+    if(!lock)
+    {
+      if(ans === question.ans)
+        {
+          
+          setScore(score + 1);
+          e.target.classList.add('correct');
+        }
+        else{
+          e.target.classList.add('incorrect');
+        }
+        setTimeout(()=>{
+          setLock(true);
+        }, 10);
+        
+    }
+  }
+
+if(isLastPage === true){
+    return(
+    <h2>Quiz score = {score}</h2>
+    )
+}
+
+  return (
+    <div className='quiz'>
+        <h1>Kod Quiz</h1>
+        <h3>{question.question}</h3>
+        <ul>
+            <li onClick={(e) => {checkAnswer(e, '1')}}>{question.option1}</li>
+            <li onClick={(e) => {checkAnswer(e, '2')}}>{question.option2}</li>
+            <li onClick={(e) => {checkAnswer(e, '3')}}>{question.option3}</li>
+            <li onClick={(e) => {checkAnswer(e, '4')}}>{question.option4}</li>
+        </ul>
+        <button onClick={nextQuestion}>Next</button>
+        <div>Question: {index + 1} of {data.length}</div>
+    </div>
+  )
+}
---
+++ src/data.js
@@ -0,0 +1,46 @@
+
+export const data=[
+    {
+        question : '5+10',
+        option1: '5',
+        option2: '10',
+        option3: '15',
+        option4: '23',
+        ans:'3'
+    },
+
+    {
+        question : '20 x 2',
+        option1: '20',
+        option2: '40',
+        option3: '60',
+        option4: '80',
+        ans:'2'
+    },
+
+    {
+        question : '3+89',
+        option1: '3',
+        option2: '98',
+        option3: '92',
+        option4: '96',
+        ans:'3'
+    },
+    {
+        question : '20-10',
+        option1: '5',
+        option2: '10',
+        option3: '15',
+        option4: '23',
+        ans:'2'
+    },
+    {
+        question : '10/2',
+        option1: '5',
+        option2: '10',
+        option3: '15',
+        option4: '23',
+        ans:'1'
+    },
+
+]
